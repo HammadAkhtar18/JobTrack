a0462220a3eecfb63fef5b1e1329e039e85dabc8
Muhammad Hammad Akhtar <hammmadakhtar10@gmail.com>
2026-04-03 15:31:27 +0500

    Add notification service for follow-up reminders

--- job_track/lib/main.dart
+++ job_track/lib/main.dart
@@ -5,12 +5,14 @@ import 'package:job_track/models/job_application.dart';
 import 'package:job_track/screens/add_application_screen.dart';
 import 'package:job_track/screens/applications_list_screen.dart';
 import 'package:job_track/screens/dashboard_screen.dart';
+import 'package:job_track/services/notification_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveFlutter.init();
   Hive.registerAdapter(JobApplicationAdapter());
   await Hive.openBox<JobApplication>('applications');
+  await NotificationService.instance.initialize();
 
   runApp(const ProviderScope(child: MyApp()));
 }
